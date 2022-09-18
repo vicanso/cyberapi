0f11b851bed381182f2c55fbb7ec2bc498c7b606
vicanso <tree.xie@outlook.com>
2022-09-18 17:21:08 +0800

    refactor: change listen addr

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() {
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
